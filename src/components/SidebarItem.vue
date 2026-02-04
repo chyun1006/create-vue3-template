4bf9868ff3b4311c653f190715aff0015e48091d
chyun1006 <746804915@qq.com>
2026-02-04 14:58:05 +0800

    feat:当子菜单只有一个的时候，不展示为折叠样式

--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -1,6 +1,19 @@
 <template>
-    <!-- 单个菜单项 -->
-    <el-menu-item v-if="!item.children" :index="item.path" @click="handleClick(item)">
+    <!-- 如果只有一个子菜单，且该子菜单没有更多子菜单，则直接展示该子菜单 -->
+    <template v-if="onlyOneChild && (!onlyOneChild.children || onlyOneChild.children.length === 0)">
+        <el-menu-item :index="onlyOneChild.path" @click="handleClick(onlyOneChild)">
+            <el-icon v-if="onlyOneChild.icon || item.icon">
+                <component :is="onlyOneChild.icon || item.icon" />
+            </el-icon>
+            <template #title>
+                <span>{{ onlyOneChild.title }}</span>
+            </template>
+        </el-menu-item>
+    </template>
+
+    <!-- 没有子菜单 -->
+    <el-menu-item v-else-if="!item.children || item.children.length === 0" :index="item.path"
+        @click="handleClick(item)">
         <el-icon v-if="item.icon">
             <component :is="item.icon" />
         </el-icon>
@@ -9,8 +22,8 @@
         </template>
     </el-menu-item>
 
-    <!-- 带子菜单的菜单项 -->
-    <el-sub-menu v-else :index="item.title">
+    <!-- 带多个子菜单的菜单项 -->
+    <el-sub-menu v-else :index="item.path || item.title">
         <template #title>
             <el-icon v-if="item.icon">
                 <component :is="item.icon" />
@@ -24,12 +37,20 @@
 
 <script setup>
 import { useRouter } from 'vue-router'
+import { computed } from 'vue'
 
-defineProps({
+const props = defineProps({
     item: {
         type: Object,
         required: true
     }
+})
+
+const onlyOneChild = computed(() => {
+    if (props.item.children && props.item.children.length === 1) {
+        return props.item.children[0]
+    }
+    return null
 })
 
 const router = useRouter()
